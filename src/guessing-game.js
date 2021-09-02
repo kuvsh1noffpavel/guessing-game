1093d6d59c6956f2862a03bdce7a52a0a266dca9
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-09-02 17:32:55 +0300

    feat: finished task guessing-game
    Finished task guessing-game. All test pasted/

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.maxValue + this.minValue)/2);
     }
 
     lower() {
-
+        this.maxValue = this.guess();
     }
 
     greater() {
-
+        this.minValue = this.guess();
     }
 }
 
